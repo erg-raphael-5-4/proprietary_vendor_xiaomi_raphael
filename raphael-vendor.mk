c2298afafb2a5a00493ee7ca01985d3e21fbe3d2
kondors1995 <normandija1945@gmail.com>
2026-01-03 19:38:01 +0000

    raphael: switch to oss audio stack

--- raphael-vendor.mk
+++ raphael-vendor.mk
@@ -163,9 +163,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/raphael/proprietary/vendor/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf
 
 PRODUCT_PACKAGES += \
-    audio.primary.raphael \
     vendor.qti.hardware.audiohalext@1.0-impl \
-    liba2dpoffload_raphael \
     libadm \
     libaudio_log_utils \
     libaudioconfigstore \
